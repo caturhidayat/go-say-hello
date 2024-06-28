4a32ea8db81365b82a1305314add03000b640b02
Catur Hidayat <caturhidayat00@gmail.com>
2024-06-28 22:26:56 +0700

    change message

--- SayHello.go
+++ SayHello.go
@@ -3,5 +3,5 @@ package go_say_hello
 import "strconv"
 
 func SayHelloBro(value string, age int) string {
-	return "Hello Bro" + value + "You are " + strconv.Itoa(age)
+	return "Hello Bro" + value + "You are " + strconv.Itoa(age) + "Now"
 }
