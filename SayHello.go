df23f09a5bd1b0f682458c5ec2611c22a5e3ae61
Catur Hidayat <caturhidayat00@gmail.com>
2024-06-29 11:35:12 +0700

    add function say goodbye

--- SayHello.go
+++ SayHello.go
@@ -5,3 +5,7 @@ import "strconv"
 func SayHelloBro(value string, age int) string {
 	return "Hello Bro " + value + "You are " + strconv.Itoa(age) + "Year old"
 }
+
+func SayGoodBye(name string) string {
+	return "Good Bye " + name
+}
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/caturhidayat/go-say-hello/v3
+module github.com/caturhidayat/go-say-hello/v4
 
 go 1.22.4
